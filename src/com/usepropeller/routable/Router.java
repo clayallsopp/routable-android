7f083714d60bd1fb1a11f0544891b59217bcafc3
Martin Marconcini <marconcini@gmail.com>
2014-12-30 14:40:27 -0800

    Fix incorrect type assignment

--- src/com/usepropeller/routable/Router.java
+++ src/com/usepropeller/routable/Router.java
@@ -371,7 +371,7 @@ public class Router {
 		RouterParams openParams = null;
 		for (Entry<String, RouterOptions> entry : this._routes.entrySet()) {
 			String routerUrl = cleanUrl(entry.getKey());
-			RouterOptions routerOptions = cleanUrl(entry.getValue());
+			RouterOptions routerOptions = entry.getValue();
 			String[] routerParts = routerUrl.split("/");
 
 			if (routerParts.length != givenParts.length) {
